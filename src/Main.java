b23fe1af5907d97aca0b88d0e609bee555671009
MariaLanss <maruska0990@gmail.com>
2023-03-16 15:02:44 +0300

    Первый билет

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,10 @@ public class Main {
     public static void main(String[] args) {
         int[] numbers = {58, 46, 301, 85, 96, 8, 555, 123, 999};
         int[] reversedNumbers = new int[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            reversedNumbers[i] = numbers[numbers.length - 1 - i];
+        int count = 0;
+        for (int i = numbers.length-1; i >= 0; i--) {
+            reversedNumbers[count] = numbers[i];
+            count++;
         }
         System.out.println(Arrays.toString(reversedNumbers));
     }
